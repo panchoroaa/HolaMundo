2ca0d6ff9580800b7a60367fd4995ff1578a33e9
panchoroaa <f.roa02@ufromail.cl>
2025-04-03 09:37:00 -0300

    Revert "fix comillas"

--- .idea/HolaMundito.java
+++ .idea/HolaMundito.java
@@ -1,6 +1,6 @@
 public class HolaMundito {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!!!");
+        System.out.println("Hola Mundo!!!!);
     }
 }
 //holamundito.java
