86f52a669c7e6dc69a288c2c1d6354a3b24fe64b
Francisco Roa <f.roa02@ufromail.cl>
2025-09-02 14:52:11 -0400

    hola mundo programa

--- src/main/java/org/example/HolaMundo.java
+++ src/main/java/org/example/HolaMundo.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!!");
+        System.out.println("Hola Mundo!!");
     }
 }
